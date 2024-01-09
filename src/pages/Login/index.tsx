25a5c09a6a9f62b1192687542cce4470d8eab354
Jadson Santos <jadsondossantos5@gmail.com>
2024-01-09 19:16:00 -0300

    feat: add error messages to Login Page and custom email validation

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,12 @@
 import { useForm } from 'react-hook-form'
 import { BsGoogle } from 'react-icons/bs'
 import { FiLogIn } from 'react-icons/fi'
+import validator from 'validator'
 
 import CustomButton from 'components/CustomButton'
 import CustomInput from 'components/CustomInput'
 import Header from 'components/Header'
+import InputErrorMessage from 'components/InputErrorMessage'
 
 import {
   LoginContainer,
@@ -42,8 +44,22 @@ const LoginPage = () => {
             <CustomInput
               hasError={!!errors?.password}
               placeholder='Digite seu e-mail'
-              {...register('email', { required: true })}
+              {...register('email', {
+                required: true,
+                validate: (value) => {
+                  return validator.isEmail(value)
+                }
+              })}
             />
+            {errors?.email?.type === 'required' && (
+              <InputErrorMessage>O e-mail é obrigatório.</InputErrorMessage>
+            )}
+
+            {errors?.email?.type === 'validate' && (
+              <InputErrorMessage>
+                Por favor, insira um e-mail válido.
+              </InputErrorMessage>
+            )}
           </LoginInputContainer>
           <LoginInputContainer>
             <p>Senha</p>
@@ -52,6 +68,9 @@ const LoginPage = () => {
               placeholder='Digite sua senha'
               {...register('password', { required: true })}
             />
+            {errors?.password?.type === 'required' && (
+              <InputErrorMessage>A senha é obrigatória.</InputErrorMessage>
+            )}
           </LoginInputContainer>
 
           <CustomButton
