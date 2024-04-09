cd18454852e6318b1ceeab06d53bc7566cf78484
Jadson Santos <jadsondossantos5@gmail.com>
2024-04-09 00:19:33 -0300

    feat: add container component

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,10 +1,11 @@
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import { useAppSelector } from 'hooks/redux.hooks'
-import { fetchCategories } from 'store/toolkit/category/category.slice'
 
 import CategoryItem from 'components/CategoryItem'
 import Loading from 'components/Loading'
+
+import { useAppSelector } from 'hooks/redux.hooks'
+import { fetchCategories } from 'store/toolkit/category/category.slice'
 
 import * as S from './Categories.styles'
 
